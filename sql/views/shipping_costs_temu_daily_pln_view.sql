188e9597b9775e3b6ae79578c3da069c4a4d2559
FilipL77 <151029519+FilipL77@users.noreply.github.com>
2026-01-14 21:09:34 +0100

    Update shipping_costs_temu_daily_pln_view.sql

--- sql/views/shipping_costs_temu_daily_pln_view.sql
+++ sql/views/shipping_costs_temu_daily_pln_view.sql
@@ -1,21 +1,29 @@
-CREATE OR REPLACE VIEW `ecommerce_db.shipping_costs_temu_daily_pln_view` AS
-WITH shipping_by_day AS (
+CREATE OR REPLACE VIEW `twinpol-ecommerce.ecommerce_db.shipping_costs_temu_daily_pln_view` AS
+WITH base AS (
   SELECT
-    s.order_date AS date,
-    SUM(c.shipping_cost_eur) AS shipping_cost_eur
-  FROM `ecommerce_db.shipping_costs_temu_clean` c
-  JOIN (
-    SELECT DISTINCT order_id, order_date
-    FROM `ecommerce_db.sales_temu_view`
-  ) s
-  ON c.order_id = s.order_id
+    DATE(date_time) AS date,
+    currency,
+    (CAST(shipping AS FLOAT64) - CAST(shipping_tax AS FLOAT64)) AS shipping_net_raw_int
+  FROM `twinpol-ecommerce.ecommerce_db.shipping_costs_temu_raw_bq`
+  WHERE currency = 'EUR'
+),
+daily_eur AS (
+  SELECT
+    date,
+    SUM(shipping_net_raw_int) / 100.0 AS shipping_cost_eur_signed
+  FROM base
   GROUP BY 1
+),
+fx AS (
+  SELECT
+    month_start_date,
+    CAST(eur_pln_avg AS FLOAT64) AS eur_pln
+  FROM `twinpol-ecommerce.ecommerce_db.fx_rates_clean`
 )
 SELECT
-  sh.date,
-  sh.shipping_cost_eur,
-  fx.eur_pln,
-  sh.shipping_cost_eur * fx.eur_pln AS shipping_cost_pln
-FROM shipping_by_day sh
-LEFT JOIN `ecommerce_db.fx_rates` fx
-  ON sh.date = fx.date;
+  d.date,
+  ABS(d.shipping_cost_eur_signed) AS shipping_cost_eur,
+  ABS(d.shipping_cost_eur_signed) * fx.eur_pln AS shipping_cost_pln
+FROM daily_eur d
+LEFT JOIN fx
+  ON DATE_TRUNC(d.date, MONTH) = fx.month_start_date;
