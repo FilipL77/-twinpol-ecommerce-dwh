b540b779042ced7fa06b05988999ea53177d8875
FilipL77 <151029519+FilipL77@users.noreply.github.com>
2026-01-14 21:12:13 +0100

    Update etl_clean_fx_rates.sql

--- sql/etl/etl_clean_fx_rates.sql
+++ sql/etl/etl_clean_fx_rates.sql
@@ -1,18 +1,8 @@
-CREATE OR REPLACE TABLE `ecommerce_db.fx_rates` AS
-WITH expanded AS (
-  SELECT
-    d AS date,
-    c.eur_pln_avg AS eur_pln
-  FROM `ecommerce_db.fx_rates_clean` c,
-  UNNEST(
-    GENERATE_DATE_ARRAY(
-      c.month_start_date,
-      DATE_SUB(DATE_ADD(c.month_start_date, INTERVAL 1 MONTH), INTERVAL 1 DAY)
-    )
-  ) AS d
-)
+CREATE OR REPLACE TABLE `ecommerce_db.fx_rates_clean` AS
 SELECT
-  date,
-  eur_pln
-FROM expanded
-WHERE eur_pln IS NOT NULL;
+  PARSE_DATE('%Y-%m', month) AS month_start_date,
+  SAFE_CAST(REPLACE(CAST(eur_pln_avg AS STRING), ',', '.') AS NUMERIC) AS eur_pln_avg,
+  source,
+  notes
+FROM `ecommerce_db.fx_rates_raw`
+WHERE month IS NOT NULL;
